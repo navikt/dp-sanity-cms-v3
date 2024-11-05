0248265c702b688d09e97754c9d3dcdc75306f39
Thor Håkon <6097177+tepose@users.noreply.github.com>
2024-11-05 09:52:26 +0100

    Rapportering message (#51)
    * Oppdater minor-avhengigheter
    * Legg til driftsmelding for rapportering

--- schema/rapportering/common-fields.ts
+++ schema/rapportering/common-fields.ts
@@ -1,6 +1,8 @@
-import { defineType } from 'sanity'
-import { textIdField } from '../soknad/common-fields'
+import { defineField, defineType } from 'sanity'
+import { alertTextField, readMoreField, textIdField } from '../soknad/common-fields'
 import { languageField } from '../common-fields'
+import { timeline } from '../soknad/timeline'
+import { BulbOutlineIcon } from '@sanity/icons'
 
 export const dynamicField = defineType({
   type: 'document',
@@ -32,4 +34,48 @@ export const dynamicField = defineType({
       title: textIdField.name,
     },
   },
+})
+
+export const richTextField = defineField({
+  type: 'array',
+  name: 'body',
+  title: 'Innhold',
+  of: [
+    {
+      type: 'block',
+      marks: {
+        annotations: [
+          {
+            name: 'link',
+            type: 'object',
+            title: 'Lenke',
+            fields: [
+              {
+                name: 'href',
+                type: 'url',
+                title: 'URL',
+              },
+              {
+                title: 'Åpne i ny fane',
+                name: 'blank',
+                type: 'boolean',
+              },
+            ],
+          },
+        ],
+      },
+      of: [
+        {
+          name: 'dynamicFieldReference',
+          type: 'reference',
+          to: [{ type: dynamicField.name }],
+          icon: BulbOutlineIcon,
+          title: 'Dynamisk felt',
+        },
+      ],
+    },
+    { type: timeline.name },
+    { type: alertTextField.name },
+    { type: readMoreField.name },
+  ],
 })
--- schema/rapportering/rapporteringRichText.ts
+++ schema/rapportering/rapporteringRichText.ts
@@ -1,7 +1,5 @@
-import { alertTextField, readMoreField, textIdField } from '../soknad/common-fields'
-import { dynamicField } from './common-fields'
-import { timeline } from '../soknad/timeline'
-import { BulbOutlineIcon } from '@sanity/icons'
+import { textIdField } from '../soknad/common-fields'
+import { richTextField } from './common-fields'
 import { languageField } from '../common-fields'
 
 export const rapporteringRichText = {
@@ -11,53 +9,7 @@ export const rapporteringRichText = {
   initialValue: {
     language: 'nb',
   },
-  fields: [
-    languageField,
-    textIdField,
-    {
-      type: 'array',
-      name: 'body',
-      title: 'Innhold',
-      of: [
-        {
-          type: 'block',
-          marks: {
-            annotations: [
-              {
-                name: 'link',
-                type: 'object',
-                title: 'Lenke',
-                fields: [
-                  {
-                    name: 'href',
-                    type: 'url',
-                    title: 'URL',
-                  },
-                  {
-                    title: 'Åpne i ny fane',
-                    name: 'blank',
-                    type: 'boolean',
-                  },
-                ],
-              },
-            ],
-          },
-          of: [
-            {
-              name: 'dynamicFieldReference',
-              type: 'reference',
-              to: [{ type: dynamicField.name }],
-              icon: BulbOutlineIcon,
-              title: 'Dynamisk felt',
-            },
-          ],
-        },
-        { type: timeline.name },
-        { type: alertTextField.name },
-        { type: readMoreField.name },
-      ],
-    },
-  ],
+  fields: [languageField, textIdField, richTextField],
   preview: {
     select: {
       title: textIdField.name,
