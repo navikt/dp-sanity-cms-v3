b37c6fe55222423d844fea79493e121058401dc7
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2025-10-21 16:16:02 +0200

    Revert "Midlertidig fiks for problemer med development dataset. Datasettet heter nå dev"
    This reverts commit 374826e36710578979642c81e5cdd6155731b106.

--- sanity.config.ts
+++ sanity.config.ts
@@ -6,7 +6,7 @@ import { structureTool } from 'sanity/structure'
 import { documentInternationalization } from '@sanity/document-internationalization'
 import { produktsideSingletonTypes } from './schema/produktside/produktsideConfig'
 
-function getAuthConfig(dataset: 'dev' | 'production'): AuthStoreOptions {
+function getAuthConfig(dataset: 'development' | 'production' | 'kopi-av-prod'): AuthStoreOptions {
   return {
     dataset,
     projectId: 'rt6o382n',
@@ -59,10 +59,10 @@ export default defineConfig([
   },
   {
     ...sharedConfig,
-    auth: createAuthStore(getAuthConfig('dev')),
-    name: 'dev',
+    auth: createAuthStore(getAuthConfig('development')),
+    name: 'development',
     title: 'Dev',
-    dataset: 'dev',
+    dataset: 'development',
     basePath: '/sanity/dev',
   },
 ])
