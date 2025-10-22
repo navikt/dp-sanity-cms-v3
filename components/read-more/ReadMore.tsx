f2f522c4e2afa732e750358f27f0aaace1e6f12b
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2025-10-22 09:42:28 +0200

    Revert ednringer i Readmore

--- components/read-more/ReadMore.tsx
+++ components/read-more/ReadMore.tsx
@@ -2,14 +2,12 @@ import React from 'react'
 import { PreviewProps } from 'sanity'
 import { TypedObject } from '@portabletext/types'
 
-// interface IProps {
-//   title: string
-//   size: 'medium' | 'small'
-//   body: TypedObject | TypedObject[]
-//   defaultOpen: boolean
-// }
-
-// Legg til kommentar for å trigge ny deploy
+interface IProps {
+  title: string
+  size: 'medium' | 'small'
+  body: TypedObject | TypedObject[]
+  defaultOpen: boolean
+}
 
 export function ReadMore(props: PreviewProps<any>) {
   console.log(props)
