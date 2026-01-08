c1a4ecef261e9e5847cbb9b719162a715fa771f4
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2026-01-08 14:09:46 +0100

    Revert "La til endring i desktructure for å teste deployment"
    This reverts commit be2f55c7bf2af05525a73f2f038a733d95a495ca.

--- desk-structure.ts
+++ desk-structure.ts
@@ -75,7 +75,7 @@ export function buildStructure(S: StructureBuilder, context: StructureResolverCo
         ),
 
       S.listItem()
-        .title('Brukerdialog - dagpenger')
+        .title('Brukerdialog')
         .child(
           S.list()
             .title('Innhold')
