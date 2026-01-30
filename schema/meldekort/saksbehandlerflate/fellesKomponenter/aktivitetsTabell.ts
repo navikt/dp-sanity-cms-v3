2cc379e8a750e8d00478014111d413ff4a2c5acb
christinaroise <christina.roise.muntean@nav.no>
2026-01-30 12:31:44 +0100

    fjern ubrukt aria felt

--- schema/meldekort/saksbehandlerflate/fellesKomponenter/aktivitetsTabell.ts
+++ schema/meldekort/saksbehandlerflate/fellesKomponenter/aktivitetsTabell.ts
@@ -100,38 +100,6 @@ export const meldekortAktivitetsTabell = defineType({
         }),
       ],
     }),
-    defineField({
-      name: 'numberInput',
-      title: 'Tallfelter (er muligens overflødig og kan fjernes)',
-      type: 'object',
-      description:
-        'Hjelpetekster for skjermlesere når man bruker pluss/minus-knapper i tallfelter.',
-      validation: (Rule) => Rule.required(),
-      group: 'numberInput',
-      fields: [
-        defineField({
-          name: 'adjustValueAriaLabel',
-          title: 'Beskrivelse av tallfelt (skjermleser)',
-          type: 'string',
-          description: 'Hjelpetekst for skjermlesere. F.eks. "Juster verdi"',
-          validation: (Rule) => Rule.required(),
-        }),
-        defineField({
-          name: 'increaseAriaLabel',
-          title: 'Beskrivelse av pluss-knapp (skjermleser)',
-          type: 'string',
-          description: 'Hjelpetekst for pluss-knappen. F.eks. "Øk"',
-          validation: (Rule) => Rule.required(),
-        }),
-        defineField({
-          name: 'decreaseAriaLabel',
-          title: 'Beskrivelse av minus-knapp (skjermleser)',
-          type: 'string',
-          description: 'Hjelpetekst for minus-knappen. F.eks. "Reduser"',
-          validation: (Rule) => Rule.required(),
-        }),
-      ],
-    }),
   ],
   preview: {
     prepare() {
